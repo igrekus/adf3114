30d006448baca7ad176356f6022d1fc8b3b692f6
Unknown <upload234@mail.ru>
2018-11-15 00:02:39 +0300

    Debug fiddle

--- progr.py
+++ progr.py
@@ -13,9 +13,9 @@ if __name__ == '__main__':
 
     reg = Adf3114InitLatch()
     print(reg)
-    reg.power_down_mode = 'normal-op'
+    reg.power_down_mode = 1
     print(reg)
-    reg.power_down_mode = 'async-pd'
+    reg.power_down_mode = 2
     print(reg)
-    reg.power_down_mode = 'sync-pd'
+    reg.power_down_mode = 3
     print(reg)
