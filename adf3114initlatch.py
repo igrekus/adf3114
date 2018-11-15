c2332aa22d43f48fcab94d50bd39c1580bbbd361
kef <upload234@mail.ru>
2018-11-15 13:42:28 +0300

    Finish init latch

--- adf3114initlatch.py
+++ adf3114initlatch.py
@@ -65,6 +65,119 @@ PD_POLARITY = {
     1: [1]
 }
 
+# 0000_0000_0000_000x_0000_0000   --   charge pump
+# F3
+#  0   0=output normal
+#  1   1=three-state
+F3 = DB8
+CHARGE_PUMP_BITS = (F3, )
+CHARGE_PUMP_MODE = {
+    0: [0],
+    1: [1]
+}
+
+# 0000_0000_0000_0xx0_0000_0000   --   fastlock mode
+# F5 | F4
+#  0    x   0=disabled
+#  1    0   1=mode 1
+#  1    1   2=mode 2
+F5, F4 = DB10, DB9
+FASTLOCK_MODE_BITS = (F5, F4)
+FASTLOCK_MODE = {
+    0: [0, 0],
+    1: [1, 0],
+    2: [1, 1]
+}
+
+# 0000_0000_0xxx_x000_0000_0000   --   timer counter control
+# TC4 | TC3 | TC2 | TC1  | timeout (PFD cycles)
+#   0     0     0     0      0=3
+#   0     0     0     1      1=7
+#   0     0     1     0      2=11
+#   0     0     1     1      3=15
+#   0     1     0     0      4=19
+#   0     1     0     1      5=23
+#   0     1     1     0      6=27
+#   0     1     1     1      7=31
+#   1     0     0     0      8=35
+#   1     0     0     1      9=39
+#   1     0     1     0      10=43
+#   1     0     1     1      11=47
+#   1     1     0     0      12=51
+#   1     1     0     1      13=55
+#   1     1     1     0      14=59
+#   1     1     1     1      15=63
+TC4, TC3, TC2, TC1 = DB14, DB13, DB12, DB11
+TIMER_COUNTER_MODE_BITS = (TC4, TC3, TC2, TC1)
+TIMER_COUNTER_MODE = {
+    0:  [0, 0, 0, 0],
+    1:  [0, 0, 0, 1],
+    2:  [0, 0, 1, 0],
+    3:  [0, 0, 1, 1],
+    4:  [0, 1, 0, 0],
+    5:  [0, 1, 0, 1],
+    6:  [0, 1, 1, 0],
+    7:  [0, 1, 1, 1],
+    8:  [1, 0, 0, 0],
+    9:  [1, 0, 0, 1],
+    10: [1, 0, 1, 0],
+    11: [1, 0, 1, 1],
+    12: [1, 1, 0, 0],
+    13: [1, 1, 0, 1],
+    14: [1, 1, 1, 0],
+    15: [1, 1, 1, 1]
+}
+
+# 0000_00xx_x000_0000_0000_0000   --   current setting 1/2
+# CPI6 | CPI5 | CPI4 |     Icp(mA)
+# CPI3 | CPI2 | CPI1 | 2.7kOhm | 4.7kOhm | 10kOhm
+#   0      0      0     1.09      0.63      0.29   0
+#   0      0      1     2.18      1.25      0.59   1
+#   0      1      0     3.72      1.88      0.88   2
+#   0      1      1     4.36      2.50      1.76   3   - ?
+#   1      0      0     5.44      3.13      1.47   4   - ?
+#   1      0      1     6.53      3.75      1.76   5   - ?
+#   1      1      0     7.62      4.38      2.06   6
+#   1      1      1     8.70      5.00      2.36   7
+CPI3, CPI2, CPI1 = DB17, DB16, DB15
+CPI6, CPI5, CPI4 = DB20, DB19, DB18
+CURRENT_SETTING_1_BITS = (CPI3, CPI2, CPI1)
+CURRENT_SETTING_2_BITS = (CPI6, CPI5, CPI4)
+CURRENT_SETTING_1 = {
+    0: [0, 0, 0],
+    1: [0, 0, 1],
+    2: [0, 1, 0],
+    3: [0, 1, 1],
+    4: [1, 0, 0],
+    5: [1, 0, 1],
+    6: [1, 1, 0],
+    7: [1, 1, 1]
+}
+CURRENT_SETTING_2 = {
+    0: [0, 0, 0],
+    1: [0, 0, 1],
+    2: [0, 1, 0],
+    3: [0, 1, 1],
+    4: [1, 0, 0],
+    5: [1, 0, 1],
+    6: [1, 1, 0],
+    7: [1, 1, 1]
+}
+
+# xx00_0000_0000_0000_0000_0000   --   prescaler value
+# P2 | P1
+#  0    0   0=8/9
+#  0    1   1=16/17
+#  1    0   2=32/33
+#  1    1   2=64/65
+P2, P1 = DB23, DB22
+PRESCALER_VALUE_BITS = (P2, P1)
+PRESCALER_VALUE = {
+    0: [0, 0],
+    1: [0, 1],
+    2: [1, 0],
+    3: [1, 1]
+}
 
 class Adf3114InitLatch(Adf3114Register):
 
@@ -112,5 +225,65 @@ class Adf3114InitLatch(Adf3114Register):
         if code not in PD_POLARITY:
             raise ValueError('Incorrect phase detector polarity code.')
         self.set_bit_pattern(code, PD_POLARITY_BITS, PD_POLARITY)
+
+    @property
+    def charge_pump_mode(self):
+        return self._find_seq(CHARGE_PUMP_BITS, CHARGE_PUMP_MODE)
+
+    @charge_pump_mode.setter
+    def charge_pump_mode(self, mode):
+        if mode not in PD_POLARITY:
+            raise ValueError('Incorrect charge pump mode.')
+        self.set_bit_pattern(mode, CHARGE_PUMP_BITS, CHARGE_PUMP_MODE)
+
+    @property
+    def fastlock_mode(self):
+        return self._find_seq(FASTLOCK_MODE_BITS, FASTLOCK_MODE)
+
+    @fastlock_mode.setter
+    def fastlock_mode(self, mode):
+        if mode not in FASTLOCK_MODE:
+            raise ValueError('Incorrect charge pump mode.')
+        self.set_bit_pattern(mode, FASTLOCK_MODE_BITS, FASTLOCK_MODE)
+
+    @property
+    def timer_counter_mode(self):
+        return self._find_seq(TIMER_COUNTER_MODE_BITS, TIMER_COUNTER_MODE)
+
+    @timer_counter_mode.setter
+    def timer_counter_mode(self, mode):
+        if mode not in TIMER_COUNTER_MODE:
+            raise ValueError('Incorrect charge pump mode.')
+        self.set_bit_pattern(mode, TIMER_COUNTER_MODE_BITS, TIMER_COUNTER_MODE)
+
+    @property
+    def current_setting_1(self):
+        return self._find_seq(CURRENT_SETTING_1_BITS, CURRENT_SETTING_1)
+
+    @current_setting_1.setter
+    def current_setting_1(self, mode):
+        if mode not in CURRENT_SETTING_1:
+            raise ValueError('Incorrect current setting 1.')
+        self.set_bit_pattern(mode, CURRENT_SETTING_1_BITS, CURRENT_SETTING_1)
+
+    @property
+    def current_setting_2(self):
+        return self._find_seq(CURRENT_SETTING_2_BITS, CURRENT_SETTING_2)
+
+    @current_setting_2.setter
+    def current_setting_2(self, mode):
+        if mode not in CURRENT_SETTING_2:
+            raise ValueError('Incorrect current setting 2.')
+        self.set_bit_pattern(mode, CURRENT_SETTING_2_BITS, CURRENT_SETTING_2)
+
+    @property
+    def prescale_value(self):
+        return self._find_seq(PRESCALER_VALUE_BITS, PRESCALER_VALUE)
+
+    @prescale_value.setter
+    def prescale_value(self, mode):
+        if mode not in PRESCALER_VALUE:
+            raise ValueError('Incorrect current setting 2.')
+        self.set_bit_pattern(mode, PRESCALER_VALUE_BITS, PRESCALER_VALUE)
 
 
