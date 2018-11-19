3a4c45ede084620a5cb24dc537727498a336c49d
kef <upload234@mail.ru>
2018-11-19 14:25:23 +0300

    Tweak UI enable on connect

--- mainwindow.py
+++ mainwindow.py
@@ -72,28 +72,34 @@ class MainWindow(QMainWindow):
         self._ui.btnDisconnect.setVisible(False)
         self._ui.btnWrite.setEnabled(False)
 
-        self._ui.ncounterLatchWidget.setEnabled(False)
-        self._ui.rcounterLatchWidget.setEnabled(False)
-        self._ui.funcLatchWidget.setEnabled(False)
-        self._ui.initLatchWidget.setEnabled(False)
+        # self._ui.ncounterLatchWidget.setEnabled(False)
+        # self._ui.rcounterLatchWidget.setEnabled(False)
+        # self._ui.funcLatchWidget.setEnabled(False)
+        # self._ui.initLatchWidget.setEnabled(False)
 
-        self._ui.editBin.setEnabled(False)
-        self._ui.editHex.setEnabled(False)
-        self._ui.editCommand.setEnabled(False)
+        # self._ui.editBin.setEnabled(False)
+        # self._ui.editHex.setEnabled(False)
+        # self._ui.editCommand.setEnabled(False)
+        # self.setBitGroupEnabled(False)
 
     def modeConnected(self):
         self._ui.btnConnect.setVisible(False)
         self._ui.btnDisconnect.setVisible(True)
         self._ui.btnWrite.setEnabled(True)
 
-        self._ui.ncounterLatchWidget.setEnabled(True)
-        self._ui.rcounterLatchWidget.setEnabled(True)
-        self._ui.funcLatchWidget.setEnabled(True)
-        self._ui.initLatchWidget.setEnabled(True)
+        # self._ui.ncounterLatchWidget.setEnabled(True)
+        # self._ui.rcounterLatchWidget.setEnabled(True)
+        # self._ui.funcLatchWidget.setEnabled(True)
+        # self._ui.initLatchWidget.setEnabled(True)
 
-        self._ui.editBin.setEnabled(True)
-        self._ui.editHex.setEnabled(True)
-        self._ui.editCommand.setEnabled(True)
+        # self._ui.editBin.setEnabled(True)
+        # self._ui.editHex.setEnabled(True)
+        # self._ui.editCommand.setEnabled(True)
+        # self.setBitGroupEnabled(True)
+
+    def setBitGroupEnabled(self, state: bool):
+        for box in self._bitGroup.buttons():
+            box.setEnabled(state)
 
     @pyqtSlot()
     def on_btnConnect_clicked(self):
